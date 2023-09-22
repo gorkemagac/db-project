125d20b92a933a8af9558913003777ca3b2bef6f
Gorkem Agac <gorkem_agac@hotmail.com>
2023-09-22 05:28:15 +0100

    db-project

---
+++ Queries/demo.sql
@@ -0,0 +1,15 @@
+select * from EMPLOYEES;
+
+select * from DEPARTMENTS;
+
+select FIRST_NAME from EMPLOYEES;
+
+select CITY from LOCATIONS;
+
+select FIRST_NAME as name from EMPLOYEES;
+
+select STREET_ADDRESS, POSTAL_CODE from LOCATIONS;
+
+select FIRST_NAME from EMPLOYEES;
+
+select distinct FIRST_NAME from EMPLOYEES;
